115640939efe1f5b8b4c1898275c3236965d908e
vraihxjehxjzhsjd <90188100+vraihxjehxjzhsjd@users.noreply.github.com>
2021-09-06 13:36:47 +0300

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,49 @@
+let config = {
+
+  // Задержка загрузки
+  preloaderDelay: 1,
+
+  // Задержка видео
+  showDelay: 1, 
+
+  // Название
+  title: 'd3pl0yx',
+
+  // Кнопочки
+  social: {
+    telegram: 'D3pl0yX',
+    youtube: 'UCa9tLAc7Fp8nB7hNk2oXvgQ',
+    github: 'D3pl0yx',
+    gift: 'DENTLY',
+  },
+
+
+
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#f5323c',
+
+
+  // Цвет кнопки Play
+  playButtonColor: '#ff0000',
+  // Тень кнопки Play
+  playButtonShadowColor: 'rgba(255, 0, 55, 0.7)',
+
+
+  // Цвет названия
+  titleColor: '#FF0000',
+  // Тень названия
+  titleShadowColor: 'rgba(255, 0, 55, 0.7)',
+
+
+  // Текст копирайта
+  copyrightText: 'soon..',
+  // Цвет копирайта
+  copyrightTextColor: '#f5323c',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#FF0000',
+  // Цвет тени социальных сетей
+  socialShadowColor: 'rgba(255, 255, 255, 0.2)',
+}
